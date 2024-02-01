f50ce032486be0b4f1822f7a7f8f4f7e835bf4ce
ME2006 <mourad7up@gmail.com>
2024-02-01 06:07:28 +0200

    .

--- l/l.py
+++ l/l.py
@@ -1,32 +1,78 @@
 from ast import literal_eval
 
-def transform_records(no_of_records, *records):
-    new_list = []
-
+def calculate_gpa(noOfRecords, records):
+    students = []
+    
     for record in records:
         parts = record.split(',,')
         name = parts[0]
-        course = parts[1]
-        grades_str = parts[2]
-        grades = literal_eval(grades_str)
+        courses = literal_eval(parts[1])
+        grades = literal_eval(parts[2])
+        
+      
+        total_gp = 0
+        for i in range(len(courses)):
+            final_grade = sum(grades[i])
+            gp = calculate_gp(final_grade)
+            total_gp += gp
+        
+        overall_gpa = round(total_gp / len(courses), 5)
+        students.append((name, overall_gpa))
+    
+    
+    students = sort_students(students)
+    
+    
+    for i in range(min(5, len(students))):
+        print(f"{students[i][0]},{students[i][1]}")
 
-        existence = False
-        for entry in new_list:
-            if entry[0] == name:
-                entry[1].append(course)
-                entry[2].append(grades)
-                existence = True
-                break
+def calculate_gp(final_grade):
+    
+    if final_grade >= 95:
+        return 4.0
+    elif final_grade >= 90:
+        return 3.9
+    elif final_grade >= 85:
+        return 3.8
+    elif final_grade >= 80:
+        return 3.7
+    elif final_grade >= 77:
+        return 3.3
+    elif final_grade >= 73:
+        return 3.0
+    elif final_grade >= 70:
+        return 2.7
+    elif final_grade >= 67:
+        return 2.3
+    elif final_grade >= 63:
+        return 2.0
+    elif final_grade >= 60:
+        return 1.7
+    elif final_grade >= 57:
+        return 1.3
+    elif final_grade >= 53:
+        return 1.0
+    elif final_grade >= 50:
+        return 0.7
+    else:
+        return 0.0
 
-        if not existence:
-            new_list.append([name, [course], [grades]])
+def sort_students(students):
 
-    for entry in new_list:
-        total_calc = 0
-        for i in entry[2].literal_eval():
-            total_calc += sum(i)
+    sorted_students = []
+    list_size = len(students)
 
-        print(f"Total GPA for {entry[0]}: {total_calc / len(entry[2])}")
+    for _ in range(list_size):
+        min_index = 0
+
+        for i in range(1, len(students)):
+            if students[i][1] > students[min_index][1]:
+                min_index = i
+
+        sorted_students.append(students.pop(min_index))
+
+    return sorted_students
+
 
 number_of_records = int(input("Enter the number of records: "))
 records = []
@@ -34,4 +80,4 @@ for i in range(number_of_records):
     record_input = input(f"Enter record {i+1}: ")
     records.append(record_input)
 
-transform_records(number_of_records, *records)
+calculate_gpa(number_of_records, records)
